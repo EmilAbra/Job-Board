29f291c08ac04dbd6829c0e329df41b2f25c594a
Emil <emilabrahamsson@yahoo.com>
2025-07-11 21:43:43 +0200

    add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSidebarClient";
+
 export default function HomePage() {
-  return <h1>HI</h1>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">EA Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>awdadw</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">main content</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
